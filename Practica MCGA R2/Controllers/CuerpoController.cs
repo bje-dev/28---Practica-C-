a3fcc52546694aa98b9b675f5e4cef8bfcc0ebf2
Bernardo Espinoza <83078404+bje-dev@users.noreply.github.com>
2023-11-13 16:09:36 -0300

    Add files via upload

---
+++ Practica MCGA R2/Controllers/CuerpoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica_MCGA_R2.Modelos;
+
+namespace Practica_MCGA_R2.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+
+    public class CuerpoController : Controller
+    {
+        public static Random random = new Random();
+
+        [HttpGet]
+        [Route("resultado")]
+        public IActionResult GetCuerpo() 
+        
+        {
+            Cuerpo _cuerpo = default;
+
+            var tipo = new string[] { "estrella", "planeta", "satelite" };
+            var temperatura = new double[] { 100, 200, 300 };
+            var tamaño = random.Next(1000, 9100);
+            var distancia = new int[] { 2500, 3600, 4988, 8542 };
+
+            _cuerpo = new Cuerpo();
+
+            _cuerpo.tipo = tipo[random.Next(tipo.Length)];
+            _cuerpo.temperatura = temperatura[random.Next(temperatura.Length)];
+            _cuerpo.tamaño = tamaño;
+            _cuerpo.distancia = distancia[random.Next(distancia.Length)];
+
+
+            
+                return Ok(_cuerpo);
+                       
+
+
+        }
+        
+       
+    }
+}
---
+++ Practica MCGA R2/Modelos/Cuerpo.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Practica_MCGA_R2.Modelos
+{
+    public class Cuerpo
+    {
+
+        public string tipo { get; set; }
+        public double temperatura { get; set; }
+        public double tamaño { get; set; }
+        public int distancia { get; set; }
+
+
+    }
+}
